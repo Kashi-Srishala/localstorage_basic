a4dd8167f991f90e999d4227d95bc9d159b85373
Kashi-Srishala <kashisrishala@gmail.com>
2023-02-14 00:10:44 +0530

    added phone number colom

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ form.addEventListener('submit', function(event){
 
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
+    var phone_no = document.getElementById('phone_no').value;
 
     localStorage.setItem('name',name);
     localStorage.setItem('email', email);
+    localStorage.setItem('phone_no', phone_no);
 });
