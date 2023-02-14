727d4e05044bb4e43e938b3402fb9c22052aecae
Kashi-Srishala <kashisrishala@gmail.com>
2023-02-14 12:06:05 +0530

    added delete function

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 //local_storage
 
+// var form = document.getElementById('form');
+// form.addEventListener('submit', function(event){
+//     event.preventDefault();
+
+//     var name = document.getElementById('name').value;
+//     var email = document.getElementById('email').value;
+//     var phone_no = document.getElementById('phone_no').value;
+
+//     localStorage.setItem('name',name);
+//     localStorage.setItem('email', email);
+//     localStorage.setItem('phone_no', phone_no);
+// });
+
+//local_storage
+
 var form = document.getElementById('form');
 form.addEventListener('submit', function(event){
     event.preventDefault();
@@ -11,4 +26,11 @@ form.addEventListener('submit', function(event){
     localStorage.setItem('name',name);
     localStorage.setItem('email', email);
     localStorage.setItem('phone_no', phone_no);
+});
+
+var deleteBtn = document.getElementById('form');
+deleteBtn.addEventListener('delete', function(event) {
+    localStorage.removeItem('name');
+    localStorage.removeItem('email');
+    localStorage.removeItem('phone_no');
 });
