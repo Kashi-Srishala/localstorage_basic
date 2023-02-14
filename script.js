89d70d63bf8440f9659fd128fe1638ddeebe94f2
Kashi-Srishala <kashisrishala@gmail.com>
2023-02-14 12:24:07 +0530

    added edit button

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-//local_storage
-
-// var form = document.getElementById('form');
-// form.addEventListener('submit', function(event){
-//     event.preventDefault();
-
-//     var name = document.getElementById('name').value;
-//     var email = document.getElementById('email').value;
-//     var phone_no = document.getElementById('phone_no').value;
-
-//     localStorage.setItem('name',name);
-//     localStorage.setItem('email', email);
-//     localStorage.setItem('phone_no', phone_no);
-// });
-
-//local_storage
-
 var form = document.getElementById('form');
 form.addEventListener('submit', function(event){
     event.preventDefault();
@@ -28,9 +11,21 @@ form.addEventListener('submit', function(event){
     localStorage.setItem('phone_no', phone_no);
 });
 
-var deleteBtn = document.getElementById('form');
-deleteBtn.addEventListener('delete', function(event) {
+var deleteBtn = document.getElementById('delete');
+deleteBtn.addEventListener('click', function(event) {
     localStorage.removeItem('name');
     localStorage.removeItem('email');
     localStorage.removeItem('phone_no');
 });
+
+var editBtn = document.getElementById('edit');
+editBtn.addEventListener('click', function(event) {
+    var name = localStorage.getItem('name');
+    var email = localStorage.getItem('email');
+    var phone_no = localStorage.getItem('phone_no');
+
+    document.getElementById('name').value = name;
+    document.getElementById('email').value = email;
+    document.getElementById('phone_no').value = phone_no;
+});
+
